e61710118d57a43869418804415bcda09e465d2a
Nurbek Ismailov <nurbek82@gmail.com>
2017-04-01 14:21:42 -0700

    finalized else part of todo

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -32,7 +32,7 @@ articleView.handleAuthorFilter = function() {
     } else {
     /* Otherwise, we should:
         1. Show all the articles except the template */
-
+        $('article:not(.template)').show();
     }
     $('#category-filter').val('');
   });
